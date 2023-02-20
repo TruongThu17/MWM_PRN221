28ae31cdba5f556419afc587289cadf98711bab0
TruongThu17 <ThuTTHE153251@fpt.edu.vn>
2023-02-20 08:55:06 +0700

    second commit

---
+++ ProjectFinal/ProjectFinal/Pages/Customer/CumtomersDebt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectFinal.Pages.Customer
+{
+    public class CumtomersDebtModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ProjectFinal/ProjectFinal/Pages/Suppiler/SupplierDebt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectFinal.Pages.Suppiler
+{
+    public class SupplierDebtModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
